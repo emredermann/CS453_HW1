9b001ed56b814493a57df953f89ee2ed84753534
6emre <emre.derman@ug.bilkent.edu.tr>
2022-03-06 18:53:54 +0300

    on

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="simple-homework-CS453_123-dictionary",
     author="Emre Derman",
-    version="2.0.0",
+    version="2.0.1",
     packages=["dictionary"],
     install_requires=[
         "requests>=2.23.0",
