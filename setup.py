075c88fb0b3f638360043d0f99edccd220f0c1f1
Emre derman <6emrederman@gmail.com>
2022-03-06 14:28:41 +0300

    Test implementation check

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="simple-homework-CS453_problematic-dictionary",
+    name="simple-homework-CS453_123-dictionary",
     author="Emre Derman",
     version="2.0.0",
     packages=["dictionary"],
